1ac7b3ea9eda413c3b27019133ec749d2bb89dae
Neil <neil.c.heather@gmail.com>
2018-06-03 23:20:06 +0000

    6-uppercase.py fix chr() and ord() 2

--- 0x02-python_if_else_loops_functions/6-uppercase.py
+++ 0x02-python_if_else_loops_functions/6-uppercase.py
@@ -3,7 +3,6 @@
 def uppercase(str):
     for c in str:
         if (ord(c) > 96) and (ord(c) < 123):
-            print(chr(ord(c) - 32), end="")
-        else:
-            print(c, end="")
+            c = chr(ord(c) - 32)
+        print(c, end="")
     print()
