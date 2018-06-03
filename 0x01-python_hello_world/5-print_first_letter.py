54c97545b7b394dedb6aae28c29ee3341111b181
Neil <neil.c.heather@gmail.com>
2018-06-03 21:15:04 +0000

    Sun Jun  3 14:14:55 PDT 2018

--- 0x01-python_hello_world/5-print_first_letter.py
+++ 0x01-python_hello_world/5-print_first_letter.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 my_word = "Holberton"
-print("{} is the first letter of the word".format(my_word[0]))
+print("{0:.1} is the first letter of the word {0}".format(my_word))
--- 0x01-python_hello_world/6-print_last_letter.py
+++ 0x01-python_hello_world/6-print_last_letter.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 my_word = "Holberton"
-print("{} is the last letter of the word".format(my_word[-1]))
+print("{} is the last letter of the word {}".format(my_word[-1], my_word))
