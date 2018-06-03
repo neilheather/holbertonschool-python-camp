c250d094cc84e8cd18e1a3300a762b4975e65032
Neil <neil.c.heather@gmail.com>
2018-06-03 23:43:22 +0000

    odd or even fix

--- 0x02-python_if_else_loops_functions/1-odd_or_even.py
+++ 0x02-python_if_else_loops_functions/1-odd_or_even.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 number = 12
 if number % 2 == 0:
-    print("{} is even\n".format(number))
+    print("{} is even".format(number))
 else:
-    print("{} is odd\n".format(number))
+    print("{} is odd".format(number))
